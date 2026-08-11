2bfc9459ca99796f83156293bd909d47d2d9b2c3
propod-op <o.pochic@gmail.com>
2026-08-11 10:43:12 +0200

    fn divide added

--- fonction.js
+++ fonction.js
@@ -9,3 +9,10 @@ function multiply(a, b) {
 function substract(a, b) {
   return a - b;
 }
+function divide(a, b) {
+  if (canDivide(a, b)) {
+    console.log("Division par zero impossible.");
+  } else {
+    return a / b;
+  }
+}
